6ccdc5d2aabbc9a836427d22eb85d2a674d4e4fa
giriprtr <46791577+giriprtr@users.noreply.github.com>
2019-02-15 15:12:45 -0500

    Add files via upload

---
+++ Exam_2_Week_10/src/m1_draw.py
@@ -0,0 +1,147 @@
+###############################################################################
+#   You are going to do a great job on this exam!
+#   Read each question carefully.
+#   Be sure to commit and push after each question
+#   Be sure to right-click on src, and Mark Directory as Sources root
+#
+#
+###############################################################################
+import rosegraphics as rg
+
+
+def main():
+    #
+    #   Tests are called from here.  Do not make any changes to main
+    #
+    test_draw_a_picture()
+
+
+def test_draw_a_picture():
+    ###############################################################################
+    #   This method tests draw_a_picture
+    #
+    ###############################################################################
+    point1 = rg.Point(150, 150)
+    n = 10
+    test_window = rg.RoseWindow(500, 500)
+
+    #  tests draw_a_picture with point 1, n=10, and test_window
+    print('###################################################')
+    print('Test 1 of draw_a_picture.')
+    print('Called with point1 =', point1)
+    print( 'n =', n, ' color = blue')
+    print('###################################################')
+    draw_a_picture(point1, n, 'blue', test_window)
+
+    point2 = rg.Point(350, 350)
+    n = 4
+    #  tests draw_a_picture with point 2, n =4, test_window
+    print('###################################################')
+    print('Test 2 of draw_a_picture.')
+    print('Called with point2 =', point2)
+    print( 'n =', n, ' color = green')
+    print('###################################################')
+    draw_a_picture(point2, n, 'green', test_window)
+    test_window.close_on_mouse_click()
+
+
+###############################################################################
+# def draw_a_picture(point, n, color, window):
+#     """
+#     See   m1_draw_problem_picture.pdf   in this project for pictures
+#     that may help you better understand the following specification:
+#
+#     What comes in:
+#       -- An rg.Point.
+#       -- A positive integer n.
+#       -- A color
+#       -- An rg.RoseWindow.
+#
+#     What goes out:  Nothing (i.e., None).
+#     Side effects:
+#       Draws an rg.Circle with the given point as the center.
+#       The radius of the rg.Circle is 100 pixels
+#       Draws an rg.Rectangle with the given point as the center.
+#       The width of the Rectangle is 160 pixels and the height is 80 pixels
+#       Draws n lines from the Center of the Rectangle to the top line
+#       of the Rectangle that are equally spaced
+#       The color is used as the line colors unless the number of the line
+#       is prime.  If the number of the line is prime,
+#       the color should be 'orange'. The fist line drawn should be
+#       the color given because one is not considered prime.
+#
+#       -- There is a 0.5 second pause after each rg.Circle is drawn.
+#       Must  ** NOT close **   the window.
+#
+#     Type hints:
+#       :type circle: rg.Circle
+#       :type n: int
+#       :type window: rg.RoseWindow
+#   The is_prime function is supplied.  Do NOT change is_prime
+#     """
+###############################################################################
+# TODO: 1  READ the doc-string for the is_prime function defined below.
+# You do NOT need to understand its implementations,
+# just its specification (per the doc-string).
+# You should  ** CALL **  functions as needed in implementing the
+# other functions.  After you have READ this, change its _TODO_ to DONE.
+###############################################################################
+
+
+def is_prime(n):
+    """
+    What comes in:  An integer n >= 1.
+    What goes out:
+      -- Returns True if the given integer is prime,
+         else returns False.
+    Side effects:   None.
+    Examples:
+      -- is_prime(11) returns  True
+      -- is_prime(12) returns  False
+      -- is_prime(2)  returns  True
+    Note: The algorithm used here is simple and clear but slow.
+    """
+    if n == 1:
+        return False
+
+    for k in range(2, (n // 2) + 1):
+        if n % k == 0:
+            return False
+
+    return True
+
+# -------------------------------------------------------------------------
+#  TODO: 2. Implement and test the draw_a_picture function.
+#           Tests have been written for you (above in main).
+#  We suggest breaking this into multiple commits.
+#     Can you show the correct circle?
+#     Can you make the correct rectangle?
+#     Are they in the correct location?
+#     Can you draw equally spaced lines?  Are they the correct color?
+#     Finally, can you make the lines whose numbers are prime orange?
+############################################################################
+# HINT:   render(0.5)
+#         renders with a half-second pause after rendering.
+############################################################################
+# -------------------------------------------------------------------------
+#
+#
+#
+def draw_a_picture(point, n, color, window):
+    circle = rg.Circle(point , 100)
+    circle.attach_to(window)
+    pt1 = rg.Point(point.x-80 , point.y-40)
+    pt2 = rg.Point(point.x+80 , point.y+40)
+    rectangle = rg.Rectangle(pt1,pt2)
+    rectangle.attach_to(window)
+    for k in range(n):
+        line = rg.Line(point , rg.Point(point.x+80-160*(k)/(n-1), point.y-40))
+        if is_prime(k+1) == True:
+            line.color = 'orange'
+        else:
+            line.color = color
+        line.attach_to(window)
+    window.render(0.5)
+
+
+main()
---
+++ Exam_2_Week_10/src/m2_bank_account.py
@@ -0,0 +1,166 @@
+#  This problem uses a class to create a bank account and simulates some
+#  typical banking practices.  Read the instructions carefully and you will be
+# successful
+def main():
+    # when you have initialized your object, use the calls below to test
+    run_test_init()
+    run_test_withdrawal()
+    return
+
+
+class Bank(object):
+    """
+    A Bank has:
+        -- NAME, a string that contains the last name of the bank account holder,
+        -- BALANCE, a float that tells the money in the account
+        -- ACCOUNT NUMBER, a string that is unique to each account holder.
+    """
+
+    def __init__(self, name, initial_deposit, account_number):
+        self.name = name
+        self.initial_deposit = initial_deposit
+        self.account_number = account_number
+        self.bal = initial_deposit
+        """
+        What comes in:
+          -- self
+          -- A string that is the name of the account holder
+          -- A float that is the initial deposit in the account
+          -- A string that is the unique account identifier
+        What is returned:
+          -- A float that is the current balance in the account
+        Side effects:
+          -- Stores the name, initial_deposit, account_number
+             in the instance variables
+          -- Also initializes other instance variables as needed
+              by other methods.
+        Examples:
+          b1 = Bank('Brackin',10000.00, A1)
+          #   b1.name is 'Brackin'
+          #   b1.balance is 10000.00
+          #   b1. account_number is 'A1'
+          b2 = Bank('Lovelace',10.15, 'A2')
+          #   b2.name is 'Lovelace'
+          #   b2.balance is 10.15
+          #   b2.account_number is 'A2'
+        Type hints:
+          :type name: str
+          :type balance: float
+          :type account_number: str
+        """
+    # ---------------------------------------------------------------------
+    # TODO: 1. Implement and test instances of this class.
+    #     See the testing code (scroll down near bottom) for more examples.
+    # ---------------------------------------------------------------------
+
+    def withdraw(self, amount):
+        """
+        What comes in:
+        -- self
+        -- A float that is the amount to be withdrawn
+        What is returned:
+        -- An error message if there is not enough money in the account
+        -- A float that is the current balance in the account if
+           there are sufficient funds
+        Side effects:
+        -- Updates the balance
+        Examples:
+          b1.withdraw(8000)
+          #   b1.name is 'Brackin'
+          #   b1.balance is 2000.00
+          #   b1. account_number is 'A1'
+          b2.withdraw(50.25)
+          #   b2.name is 'Lovelace'
+          #   b2.balance is 10.15 (no money is withdrawn)
+          #   b2.account_number is 'A2'
+          #   an error message is printed because there are insufficient funds
+        """
+    # ---------------------------------------------------------------------
+    # TODO: 4. Implement and test the withdraw method
+    #     Implement your own test code, before you write your method
+    #     Insert your test code for withdraw, where indicated
+    #     Scroll down near the bottom of this screen
+    # ---------------------------------------------------------------------
+    #   Put your code for withdraw below
+    #
+    # ---------------------------------------------------------------------
+        self.bal = self.bal - amount
+        if self.bal >= 0:
+            return self.bal
+        else:
+            return ("ValueError")
+
+
+def run_test_init():
+    """ Tests the   __init__   method of the Bank class. """
+    print()
+    print('-----------------------------------------------------------')
+    print('Testing the   __init__   method of the Bank class.')
+    print('-----------------------------------------------------------')
+
+    # Test 1:  Contents fit in the Box easily.
+    b1= Bank('Brackin', 10000, 'A1')
+    expected_name = 'Brackin'
+    expected_balance = 10000
+    expected_account_number = 'A1'
+    print("Expected:", expected_name, expected_balance, expected_account_number)
+    print("Actual:  ", b1.name, b1.bal, b1.account_number)
+    if (expected_name == b1.name) and (expected_balance == b1.bal) and (expected_account_number == b1.account_number):
+        print("Test passed SUCCESSFULLY!")
+    else:
+        print_failure_message()
+    print()
+    # ---------------------------------------------------------------------
+    # TODO: 2. Add two more test cases for your Bank class below.
+    # ---------------------------------------------------------------------
+    b2 = Bank('Lovelace', 10.15, 'A2')
+    expected_name = 'Lovelace'
+    expected_balance = 10.15
+    expected_account_number = 'A2'
+    print("Expected:", expected_name, expected_balance, expected_account_number)
+    print("Actual:  ", b2.name, b2.bal, b2.account_number)
+    if (expected_name == b2.name) and (expected_balance == b2.bal) and (
+            expected_account_number == b2.account_number):
+        print("Test passed SUCCESSFULLY!")
+    else:
+        print_failure_message()
+    print()
+    b3 = Bank('Joe', 100, 'A3')
+    expected_name = 'Joe'
+    expected_balance = 100
+    expected_account_number = 'A3'
+    print("Expected:", expected_name, expected_balance, expected_account_number)
+    print("Actual:  ", b3.name, b3.bal, b3.account_number)
+    if (expected_name == b3.name) and (expected_balance == b3.bal) and (
+            expected_account_number == b3.account_number):
+        print("Test passed SUCCESSFULLY!")
+    else:
+        print_failure_message()
+    print()
+# ---------------------------------------------------------------------
+# TODO: 3. Implement your test for the withdraw method below
+# ---------------------------------------------------------------------
+def run_test_withdrawal():
+    b1 = Bank('Brackin', 10000, 'A1')
+    print('Expected = ', 9000)
+    print('Actual = ', b1.withdraw(1000))
+    print()
+
+    b3 = Bank('Brady', 100, 'A3')
+    b3.withdraw(200)
+    print('Expected = ValueError' )
+    print('Actual = ', b3.withdraw(200))
+    print()
+
+# Implement at least two tests.  Use copy and paste to speed your coding.
+
+
+
+def print_failure_message():
+    print('  *** FAILED the above test. ***')
+
+
+# -----------------------------------------------------------------------------
+# Calls  main  to start the ball rolling.
+# ------------------------------------------------------------------------
+main()
